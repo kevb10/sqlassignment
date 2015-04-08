89fe9c29279f9f6797245ccd13a3f53139087f39
kevb10 <kevinmanase@k-state.edu>
2015-04-08 17:29:19 -0500

    changes suggested by SF

--- my_monthname.c
+++ my_monthname.c
@@ -20,22 +20,29 @@ extern void my_monthname(sqlite3_context  *context,
                          sqlite3_value   **argv){
     
     char *str =  (char *)sqlite3_value_text(argv[1]);
-    char *month_char = strtok(str, "/.- ");
-    char *day_char = strtok(NULL, "/.- ");
-    char *year_char = strtok(NULL, "/.- ");
+  /*  char *str2 = strdup(str);
+  */
     
-    char *themonth = NULL;
-        
+    char *month_char;
+    char *day_char;
+    char *year_char;
+    
     char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August",
         "September", "October", "November", "December"};
     
-    int month = atoi(month_char);
-    int day = atoi(day_char);
-    int year = atoi(year_char);
     
-    if (argc != 1){
+    if (str == NULL)
+    {
+        sqlite3_result_null(context);
+    }
+    
+    if (3 != sscanf(str, "%c/%c/%c",
+                    year_char, month_char, day_char)) {
         sqlite3_result_error(context, "monthname - invalid number of arguments", -1);
     } else {
+        int month = atoi(month_char);
+        int day = atoi(day_char);
+        int year = atoi(year_char);
         // Check whether at least one parameter is null
         if ((sqlite3_value_type(argv[0]) == SQLITE_NULL)
             || (sqlite3_value_type(argv[1]) == SQLITE_NULL)) {
@@ -43,13 +50,10 @@ extern void my_monthname(sqlite3_context  *context,
             return;
         }
         // Check whether the length is a positive value
-        if ((sqlite3_value_numeric_type(argv[1]) != SQLITE_INTEGER)
-            || ((len = sqlite3_value_int(argv[1])) <= 0)) {
-            sqlite3_result_error(context, "monthname - invalid format. Please use mm/dd/yyyy", -1);
+        if (sqlite3_value_numeric_type(argv[1]) != SQLITE_INTEGER) {
+            sqlite3_result_error(context, "monthname - invalid format. Please use YYYY-MM-DD", -1);
             return;
         }
-        themonth = (char *)sqlite3_malloc(sizeof(char)*128);
-        themonth = months[month];
-        sqlite3_result_text(context, themonth, strlen(themonth)+256, free);
+        sqlite3_result_text(context, months[month], strlen(months[month]), SQLITE_STATIC);
     }
 }
