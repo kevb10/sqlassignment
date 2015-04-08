54cf9bf99344fb1659040e99ab05e692f46ec082
kevb10 <kevinmanase@k-state.edu>
2015-04-08 18:01:48 -0500

    cleaned up code

--- my_monthname.c
+++ my_monthname.c
@@ -20,12 +20,10 @@ extern void my_monthname(sqlite3_context  *context,
                          sqlite3_value   **argv){
     
     char *str =  (char *)sqlite3_value_text(argv[1]);
-  /*  char *str2 = strdup(str);
-  */
     
-    char *month_char;
-    char *day_char;
-    char *year_char;
+    char *month_char = "MM";
+    char *day_char = "DD";
+    char *year_char = "YYYY";
     
     char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August",
         "September", "October", "November", "December"};
@@ -36,24 +34,14 @@ extern void my_monthname(sqlite3_context  *context,
         sqlite3_result_null(context);
     }
     
-    if (3 != sscanf(str, "%c/%c/%c",
+    if (3 != sscanf(str, "%c-%c-%c",
                     year_char, month_char, day_char)) {
         sqlite3_result_error(context, "monthname - invalid number of arguments", -1);
     } else {
         int month = atoi(month_char);
         int day = atoi(day_char);
         int year = atoi(year_char);
-        // Check whether at least one parameter is null
-        if ((sqlite3_value_type(argv[0]) == SQLITE_NULL)
-            || (sqlite3_value_type(argv[1]) == SQLITE_NULL)) {
-            sqlite3_result_null(context);
-            return;
+        
+        sqlite3_result_text(context, months[month], strlen(months[month])+256, SQLITE_STATIC);
         }
-        // Check whether the length is a positive value
-        if (sqlite3_value_numeric_type(argv[1]) != SQLITE_INTEGER) {
-            sqlite3_result_error(context, "monthname - invalid format. Please use YYYY-MM-DD", -1);
-            return;
-        }
-        sqlite3_result_text(context, months[month], strlen(months[month]), SQLITE_STATIC);
-    }
 }
